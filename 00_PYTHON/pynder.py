24c497ff6ca630a857177460b8931877eab6cbfa
18826700 <ALeoKolesnikov@sberbank.ru>
2021-09-13 10:01:52 +0300

    Теория

--- 00_PYTHON/pynder.py
+++ 00_PYTHON/pynder.py
@@ -25,6 +25,8 @@ x = 10
 
 swipe_first = '//*[@id="c-690079234"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[4]'
 swipe = '//*[@id="c-690079234"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[5]/div/div[4]'
+swipe_dislike = '//*[@id="c-690079234"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[5]/div/div[2]'
+
 close_total_tinder = '//*[@id="c1564682258"]/div/div/button[2]'
 cl2 = '//*[@id="c1564682258"]/div/div/div[2]/button[2]'
 
@@ -32,6 +34,7 @@ swipe_mobile = '//*[@id="c-690079234"]/div/div[1]/div/div/main/div/div[1]/div[1]
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://tinder.com')
+
 
 
 def tryCloseAll(i):
@@ -46,12 +49,17 @@ def tryCloseAll(i):
         return
     except:
         pass
+    try:
+        driver.find_element_by_xpath(swipe_first).click()
+        return
+    except:
+        pass
     return
 
 
 options = {
-    'error_max': 10,
-    'swipes': 1000,
+    'error_max': 30,
+    'swipes': 3000,
     'timer_second': 2,
 }
 
@@ -59,30 +67,39 @@ def autoLikes(options):
     error_signals_max = options['error_max']
     error_signals = error_signals_max
     i = options['swipes']
-    matches = 0
+    dislikes = 0
     start_time = datetime.datetime.today()
-    send('Starting process... (' + str(len(driver.find_elements_by_class_name('matchListItem')) - 2) + ')')
+    matches = 0
+    swipe_sum = 0
     while i > 0:
         try:
+            matches = len(driver.find_elements_by_class_name('matchListItem')) - 2
             if i == options['swipes']:
                 print(i)
                 driver.find_element_by_xpath(swipe_first).click()
+                i -= 1
+                swipe_sum += 1
             if i < options['swipes']:
-                print(i)
-                driver.find_element_by_xpath(swipe).click()
-            i -= 1
-            newMatches = len(driver.find_elements_by_class_name('matchListItem')) - 2
-            if newMatches > matches and matches > 0:
-               matches = newMatches
-               send(newMatches)
+                if (random.random() < 0.90 + random.random()/10):
+                    print(i)
+                    driver.find_element_by_xpath(swipe).click()
+                    i -= 1
+                    swipe_sum += 1
+                else:
+                    print('dislike')
+                    driver.find_element_by_xpath(swipe_dislike).click()
+                    dislikes = dislikes + 1
             error_signals = error_signals_max
         except:
             error_signals -= 1
+            if error_signals == 1:
+                driver.get('https://tinder.com/app/recs')
+                time.sleep(60)
             if error_signals == 0:
-                send('Твой бот сломался :(')
-                send('Свайпов: ' + str(matches) + '\nВремя сеанса: ' + str(datetime.datetime.today() - start_time))
-                error_signals = math.inf
+                send('Error...')
+                i = 0
             tryCloseAll(i)
         time.sleep(options['timer_second'] + random.random())
-    send('Свайпов: ' + str(matches) + '\nВремя сеанса: ' + str(datetime.datetime.today() - start_time))
+    send('Свайпов: ' + str(swipe_sum) + '\nДизлайков: ' + str(dislikes) + '\nИтого открытых матчей: ' + str(matches) + '\nВремя сеанса: ' + str(datetime.datetime.today() - start_time))
+
 
